8db53d83eb3b5df119058fc666c6f52cd4a25e3b
aftery <m13115766518@163.com>
2019-10-30 18:11:30 +0800

    spring-mybatis

---
+++ springboot-perproties/src/main/java/top/aftery/PerprotiesApplication.java
@@ -0,0 +1,15 @@
+package top.aftery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+@SpringBootApplication
+public class PerprotiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerprotiesApplication.class, args);
+    }
+
+}
